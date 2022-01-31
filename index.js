4cf3d9cbb29f82c1e4c98fcdf654c0fab1cb9840
sebasapon96 <74080212+sebasapon96@users.noreply.github.com>
2022-01-31 09:58:49 -0500

    cambias con ws para realizar post

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const multer = require('multer');
 const cors = require('cors');
 const app = express();
 const ftp = require('basic-ftp');
+const res = require('express/lib/response');
 
  require('dotenv').config();
     
-  async function sendFiles() {
+  async function sendFiles(rFile,nFile) {
     
     const client = new ftp.Client();
     // Only for debug session
@@ -37,19 +38,84 @@ const ftp = require('basic-ftp');
         let destinationPath = process.env.FTPS_DIR;
         
       
-         await client.downloadTo('dowloads/' + process.env.FTPS_NAMEDOC,destinationPath + process.env.FTPS_NAMEDOC);
+         await client.downloadTo('dowloads/' + nFile,rFile + nFile);
+       
          
        
     } catch (err) {
         console.error(err);
+        
     }
    client.close();  
 }
- 
-    sendFiles().then(v =>{
+
+app.use(cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Authorization, Origin, X-Requested-With, Accept, Cache-Control",
+    preflightContinue: true,
+    optionsSuccessStatus: 204,
+}));
+
+// Enable pre-flight across-the-board
+app.options('*', cors(), ((req, res, next) => {
+    console.log(req, res, next);
+    res.send('CORS Allow');
+}));
+
+app.post('/service/ftp/tyj/fidu',  function (req, res, next) {
+    console.log("Getting POST request from: ", req.ip, new Date().toISOString())
+
+    // Verify the pre-condition, the params Taylor-Param1 and
+    // Taylor-Param2 must be exist in the header of HTTP
+    if (req.headers["ruta-ftps"] === undefined) {
+        // 500 Internal Server Error
+        return res.status(500).send("The ruta-ftps not send in the header of HTTP");
+    }
+
+    if (req.headers["name-file"] === undefined) {
+        // 500 Internal Server Error
+        return res.status(500).send("The name-file not send in the header of HTTP");
+    }
+    
+    if (req.headers["ruta-ftps"] === "") {
+        // 500 Internal Server Error
+        return res.status(500).send("The ruta-ftps not send in the header of HTTP");
+    }
+
+    if (req.headers["name-file"] === "") {
+        // 500 Internal Server Error
+        return res.status(500).send("The name-file not send in the header of HTTP");
+    }
+    const rutaFile = req.headers["ruta-ftps"] ;
+    // @type List[String] List of words in the body.
+    const nameFile = req.headers["name-file"];
+    
+    sendFiles(rutaFile,nameFile).then(v =>{
+        try{
+            res.download('dowloads/' + nameFile,nameFile);  
+        }
+        catch{
+            res.send("no archivo");
+        }
 
     });
+    
+   // res.download('dowloads/' + nameFile,nameFile);
+    
+    
+})
+ 
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+    
 
 
     
+  
   
